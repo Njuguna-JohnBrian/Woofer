812a9e4e144442fa14a719eb5960c34ea3c70345
John Brian <njugunajb96@gmail.com>
2023-09-24 16:36:00 +0300

    OnboardingStrings

---
+++ lib/screens/onboarding/onboarding_utils/onboarding_strings.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/cupertino.dart';
+
+@immutable
+class OnboardingStrings {
+  static const scrn1_text = "Let's create a better life for a mini you";
+  static const scrn1_img = "assets/images/dog1.png";
+
+  static const scrn2_text = "It's not a home without a dog";
+  static const scrn2_img = "assets/images/dog_care.png";
+
+  static const scrn3_text = "Bringing a community together";
+  static const scrn3_img = "assets/images/dog2.png";
+
+  static const next = "Next";
+  static const skip = "Skip";
+
+  const OnboardingStrings._();
+}
