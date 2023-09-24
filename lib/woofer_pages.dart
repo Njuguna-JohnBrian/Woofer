0f993857ebbe606e96344e518e4e01d906c8ceef
John Brian <njugunajb96@gmail.com>
2023-09-24 20:43:44 +0300

    wooferPages

---
+++ lib/woofer_pages.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+import 'package:woofer/screens/selection/selection.dart';
+
+import 'coming_soon.dart';
+
+List<Widget> wooferPages = <Widget>[
+  SelectionScreen(),
+  ComingSoonScreen(),
+  ComingSoonScreen(),
+];
